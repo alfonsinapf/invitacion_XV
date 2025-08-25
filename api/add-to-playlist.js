12c23f0ff6f965d4d30e9b6ec2f7b0a6764b124f
alfonsinapf <alfonsinapanzafontana@gmail.com>
2025-08-25 19:16:26 -0300

    Update add-to-playlist.js

--- api/add-to-playlist.js
+++ api/add-to-playlist.js
@@ -1,13 +1,12 @@
 export default async function handler(req, res) {
   // Configurar CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
+ if (req.method === 'OPTIONS') {
+  return res.status(200).end();
+}
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
