404b4633c2f4d981eb8f9797a10a0ba2f3fd63ce
alfonsinapf <alfonsinapanzafontana@gmail.com>
2025-08-26 13:20:53 -0300

    Update add-to-playlist.mjs

--- api/add-to-playlist.mjs
+++ api/add-to-playlist.mjs
@@ -31,7 +31,7 @@ console.log("Client ID:", process.env.CLIENT_ID);
 console.log("Client Secret:", process.env.CLIENT_SECRET ? "EXISTS" : "MISSING");
 
 const authString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
-const auth = Buffer.from(authString).toString("base64");
+const authHeader = Buffer.from(authString).toString("base64");
 console.log("Auth header:", auth);
     
     const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
