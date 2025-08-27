900216af34a7127577f1381727daf6c34f4778b1
alfonsinapf <alfonsinapanzafontana@gmail.com>
2025-08-27 18:10:26 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const params = new URLSearchParams();
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "https://invitacion-xv-seven.vercel.app/api/callback");
+  params.append("redirect_uri", "https://invitacion-xv-seven.vercel.app/callback");
   params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
   params.append("client_secret", process.env.SPOTIFY_CLIENT_SECRET);
 
