1c9e8b60f899d6662b467edc4cb36f068abf682e
alfonsinapf <alfonsinapanzafontana@gmail.com>
2025-09-09 08:59:45 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const scopes = [
-    "user-read-email",      // leer email (opcional)
+    "user-read-email",
     "playlist-read-private",
     "playlist-modify-public",
     "playlist-modify-private",
@@ -10,11 +10,10 @@ export default function handler(req, res) {
     response_type: "code",
     client_id: process.env.SPOTIFY_CLIENT_ID,
     scope: scopes,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI, // debe coincidir con el de tu app Spotify
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
   });
 
   const authUrl = `https://accounts.spotify.com/authorize?${queryParams.toString()}`;
 
-  // 🔀 Redirigir al usuario a Spotify
   res.redirect(authUrl);
 }
