4f2c27e16a56a8624bc1d44a270b616db8e98569
alfonsinapf <alfonsinapanzafontana@gmail.com>
2025-09-09 16:51:29 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// firebaseAdmin.js
+const admin = require("firebase-admin");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCewDiHzRv18_FnmBzuyo00rgo__9HtZKc",
-  authDomain: "libro-xv-alfonsina.firebaseapp.com",
-  projectId: "libro-xv-alfonsina",
-  storageBucket: "libro-xv-alfonsina.firebasestorage.app",
-  messagingSenderId: "793654895943",
-  appId: "1:793654895943:web:55db375ccf9b439b761862"
-};
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
+}
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = admin.firestore();
+module.exports = db;
